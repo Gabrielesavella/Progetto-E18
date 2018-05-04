ebf5ffc43fe2684d924879818dcb6499ba1935c3
gabriele savella <gabrielesavella@gmail.com>
2018-05-04 19:52:33 +0200

    Aggiunta di metodi get

--- src/persone/Invitato.java
+++ src/persone/Invitato.java
@@ -1,15 +1,34 @@
-package persone;
-
+/*@author Marco Maffoni,Gabriele Savella*/
 public class Invitato {
 
-    private String codiceFiscale,nome,cognome;
+    private String cf,nome,cognome;
     private int età;
-    
-    public Invitato (String codiceFiscale,String nome,String cognome, int età){
-        this.codiceFiscale=codiceFiscale;
-        this.età=età;
-        this.nome=nome;
-        this.cognome=cognome;
+    /*
+    @parametri:
+    cf è il codice fiscale dell'invitato
+    nome è il nome dell'invitato
+    cognome è il cognome dell'invitato
+     */
+    public Invitato (String cf,String nome,String cognome,int età){
+        this.cf = cf;
+        this.cognome = cognome;
+        this.nome = nome;
+        this.età = età;
     }
-       
+
+    public String getCf() {
+        return cf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public int getEtà() {
+        return età;
+    }
 }
