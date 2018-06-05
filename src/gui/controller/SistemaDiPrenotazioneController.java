8b609e0d39fb845b3532b69a2fdcea3db833d0b1
marcolecce <lecciovich95@live.it>
2018-06-05 17:43:00 +0200

    3rdCommit. Added Sistema di prenotazione Controller

--- src/gui/controller/SistemaDiPrenotazioneController.java
+++ src/gui/controller/SistemaDiPrenotazioneController.java
@@ -1,4 +1,27 @@
 package gui.controller;
 
-public class SistemaDiPrenotazioneController {
+public class SistemaDiPrenotazioneController{
+    private boolean loggedIn=false;
+
+    public boolean signUp(String nome,String cognome,String email,String username,String password){
+
+        return true;
+    }
+
+    public boolean login(String username,String password){
+        return true;
+    }
+
+    public boolean logout(){
+        return true;
+    }
+
+    public boolean creaEvento(){
+        return true;
+    }
+
+    public boolean acquisisciInvitati(){
+        return true;
+    }
+
 }
