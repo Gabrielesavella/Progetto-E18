9fd89ceb1ccc233bd5da561bf6d1a0d64dacc968
Gabrielesavella <gabriele.savella@gmail.com>
2018-06-06 15:50:12 +0200

    implementazioni di metodi

--- src/AbstractFacade.java
+++ src/AbstractFacade.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class AbstractFacade {
@@ -12,10 +13,19 @@ public class AbstractFacade {
     }
 
     private void WriteGuests(String fiscaleCode,String nameGuest, String surnameGuest,int age){
+        field.add(fiscaleCode);
+        field.add(nameGuest);
+        field.add(surnameGuest);
+        field.add(Integer.toString(age));
+
 
     }
 
     private void WriteEvent(String nameEvent, GregorianCalendar dateEvent,int guestNumber){
+       //promemoria per @author Gabrielesavella : da convertire dateevent in string (ora mancanzi di tempo causa lezione
+        field.add(nameEvent);
+        field.add(Integer.toString(dateEvent.get(Calendar.DATE)));
+        field.add(Integer.toString(guestNumber));
 
     }
 }
