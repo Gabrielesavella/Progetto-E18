35c32bfb372e10408ecf12a9df5bbaba9a7be265
Gabrielesavella <gabriele.savella@gmail.com>
2018-06-06 17:10:22 +0200

    aggiunta metodo generate, gestione errori (livello minimo) per i metodi di questa superclasse ( in vista della gestione degli errori dele classe figlie )

--- src/AbstractFacade.java
+++ src/AbstractFacade.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -5,27 +6,44 @@ import java.util.GregorianCalendar;
 public class AbstractFacade {
     protected ArrayList <String> field = new ArrayList<String>();
 
-    private void WriteClient(String name, String surname, String email, String password){
+    public void WriteClient(String name, String surname, String email, String password)throws IOException{
         field.add(name);
         field.add(surname);
         field.add(email);
         field.add(password);
+        generate();
     }
 
-    private void WriteGuests(String fiscaleCode,String nameGuest, String surnameGuest,int age){
+    public void WriteGuests(String fiscaleCode,String nameGuest, String surnameGuest,int age) throws IOException{
         field.add(fiscaleCode);
         field.add(nameGuest);
         field.add(surnameGuest);
         field.add(Integer.toString(age));
+        generate();
 
 
     }
 
-    private void WriteEvent(String nameEvent, GregorianCalendar dateEvent,int guestNumber){
+    public void WriteEvent(String nameEvent, GregorianCalendar dateEvent,int guestNumber)throws IOException{
        //promemoria per @author Gabrielesavella : da convertire dateevent in string (ora mancanzi di tempo causa lezione
         field.add(nameEvent);
         field.add(Integer.toString(dateEvent.get(Calendar.DATE)));
         field.add(Integer.toString(guestNumber));
+        generate();
 
+    }
+    /*
+    questo è il metodo che andrà a generare il file (ovviamente non posso implementarlo perchè questo metodo andrà
+    cambiato in base alla classe ereditata, finito di generare il file svuoto il campo (visto che è protected
+     */
+
+    public void generate()throws IOException {
+
+        field.clear();
+
+    }
+
+    public ArrayList<String> getField() {
+        return field;
     }
 }
