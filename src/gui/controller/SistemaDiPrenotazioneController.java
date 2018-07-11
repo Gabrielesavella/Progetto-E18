6d3efa217d5872121b4dac19f3219919c30f5351
Marco130222 <marco.maffoni01@universitadipavia.it>
2018-07-11 12:12:41 +0200

    Add src/

---
+++ src/gui/controller/SistemaDiPrenotazioneController.java
@@ -0,0 +1,178 @@
+package gui.controller;
+
+
+import facade.*;
+import locale.GestoreEvento;
+import persone.Cliente;
+import persone.Invitato;
+import vincoli.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import database.ConnessioneDB;
+
+/**
+ *
+ * @author lecciovich
+ */
+
+
+public class SistemaDiPrenotazioneController{
+    private boolean loggedIn=false,notdone = true;
+    private AbstractFacade facade;
+    private XlsFacade xlsFacade;
+    private ConnessioneDB connessione ;
+    ArrayList<Invitato> listainvitatiEvento=null;
+
+    public SistemaDiPrenotazioneController(){
+        connessione = new ConnessioneDB();
+        try {
+            facade=new txtFacade(1);
+            xlsFacade=new XlsFacade();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //aggiunta connessione DB, se non c'è il cliente registrato , allora lo inserisco in registrazioni sul txt e nel db
+    public boolean signUp(String name,String surname,String email,String username,String password){
+        try {
+            Cliente fetching=facade.fetchClient(username,password);
+            if (fetching!=null){
+                System.out.println("found client with same username. Please try again with a different one.");
+                return false;
+            }
+            ConnessioneDB connessione = new ConnessioneDB();
+            connessione.inserisciDatiCliente(username,name,surname,email,password);
+            facade.WriteClient(username,password,name,surname,email);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public Cliente login(String username, String password){
+        Cliente fetching= null;
+        try {
+            fetching = facade.fetchClient(username,password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (fetching!=null) { loggedIn = true; }
+        return fetching;
+    }
+
+
+    public void logout(){
+        loggedIn=false;
+    }
+
+    //adattato al DB inserisce l'evento anche nel DB (controllare la data!)
+    public boolean creaEvento(String nomeEvento, GregorianCalendar data, int guestNum, Cliente cliente,String nomelocale){
+        String datadb = data.get(Calendar.DAY_OF_MONTH)+"-"+data.get(Calendar.MONTH)+"-"+data.get(Calendar.YEAR);
+        System.out.println(datadb);
+        try {
+            facade.WriteEvent(nomeEvento,data,guestNum);
+            //ho bisogno del nome locale
+            connessione.inserisciDatiEvento(cliente.getUsername(),nomeEvento,datadb,nomelocale,guestNum);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public GestoreEvento getEvento(String nomeEvento){
+        String[] columns= new String[10];
+        GestoreEvento gestoreEvento =null;
+        try {
+            gestoreEvento =facade.fetch(nomeEvento,columns);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return gestoreEvento;
+    }
+
+
+    public boolean acquisisciInvitati(ArrayList<Invitato> invitati){
+        for (Invitato i:invitati) {
+            try {
+                facade.WriteGuests(i.getID_Inv(),i.getNome(),i.getCognome(),i.getEta());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<Invitato> getInvitati(){
+        ArrayList<Invitato> invitati= null;
+        try {
+            txtFacade txtFacade= new txtFacade(1);
+            invitati.addAll(txtFacade.fetchAllGuests());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return invitati;
+    }
+
+    public boolean createXlsGenerality(String nomeEvento){
+        xlsFacade.generateXlsGuests(nomeEvento);
+        try {
+            xlsFacade.openfile(nomeEvento+".xls");
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public ArrayList<Invitato> loadXlsGenerality(String nomeEvento){
+
+        ConnessioneDB connessione = new ConnessioneDB();
+        try {
+            listainvitatiEvento = xlsFacade.readXlsGuests(nomeEvento);
+
+        int sizelist = listainvitatiEvento.size();
+        //System.out.println("numero di elementi della lista: "+sizelist);
+        txtFacade t = new txtFacade(sizelist);
+        //txtFacade t = new txtFacade(nomeEvento+"lista_invitati.txt",sizelist);
+            if(this.notdone)
+        for (Invitato element:listainvitatiEvento) {
+            t.WriteGuests(element.getID_Inv(),element.getNome(),element.getCognome(),element.getEta());
+            this.notdone=false;
+            connessione.inserisciDatiInvitato(nomeEvento,element.getID_Inv(),element.getNome(),element.getCognome(),element.getEta());
+        }
+        }catch (IOException e){ e.printStackTrace(); }
+
+        return listainvitatiEvento ;
+    }
+    //NOTA BENE: da correggere
+    public ArrayList<Invitato> writeXlsObligations(String nomeEvento){
+        ArrayList<Invitato> result = new ArrayList<Invitato>();
+        if (!xlsFacade.reWriteXls(nomeEvento,loadXlsGenerality(nomeEvento))){
+            return null;
+        }
+        //facendo così lo richiami + di una volta , 3 se inserisci  il numero di invitati
+        //result = loadXlsGenerality(nomeEvento);
+        this.notdone=true;
+        return listainvitatiEvento;
+    }
+
+    public ArrayList<SpecificaTavolo> saveOnObligations(String nomeEvento){
+        ArrayList<SpecificaTavolo> specificaTavolos=xlsFacade.readSpecificheTavolo(nomeEvento);
+//        if (specificaTavolos==null){
+//            System.out.println("no buono");
+//        }
+//        System.out.println("yuppi");
+        return specificaTavolos;
+    }
+
+}
+
---
+++ src/vincoli/CreatePreferenza.java
@@ -0,0 +1,83 @@
+package vincoli;
+
+import database.ConnessioneDB;
+import locale.Evento;
+import locale.Tavolo;
+import persone.Invitato;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class CreatePreferenza {
+
+    ConnessioneDB c;
+    ArrayList<PreferenzaInvitato> pref = new ArrayList<>();
+    ArrayList<GestorePreferenzaInvitato> gpref = new ArrayList<>();
+    ArrayList<Invitato> invitati = new ArrayList<>();
+    ArrayList<Evento> eventi = new ArrayList<>();
+    Evento ev;
+    ArrayList<Tavolo> tavoli = new ArrayList<>();
+    ArrayList<GestorePreferenzaInvitato> gp = new ArrayList<>();
+
+
+    public CreatePreferenza(String ID_Ev, ArrayList<Tavolo> tav){
+
+        c=new ConnessioneDB();
+        c.startConn();
+
+        this.invitati = c.getInvitato(ID_Ev);
+        this.ev=c.getEventoSingolo(ID_Ev);
+        pref= c.getVincoloInvitato(ID_Ev);
+        c.closeConn();
+
+        this.tavoli = tav;
+        gpref= crea();
+    }
+
+    public ArrayList<Tavolo> getTavoli() {
+        return tavoli;
+    }
+
+
+    public ArrayList<GestorePreferenzaInvitato> crea(){
+
+        for (PreferenzaInvitato p : pref){
+            p.setEvento(ev);
+            p.addInvitati(invitati);
+            p.getTavoli().addAll(tavoli);
+            gp.addAll(p.gestisciPreferenza());
+
+        }
+
+        return gp;
+    }
+
+    public ArrayList<GestorePreferenzaInvitato> getGpref() {
+        return gpref;
+    }
+
+    public void smista(){
+
+        for (GestorePreferenzaInvitato gpi : gp){
+
+            if(gpi.getPreferenza()==PreferenzaInvitatoEnum.STA_VICINO_A) {
+
+                gpi.verificaIdoneita();
+            }
+        }
+
+        for (GestorePreferenzaInvitato gpi : gp){
+
+            if(gpi.getPreferenza()==PreferenzaInvitatoEnum.NON_STA_VICINO_A) {
+
+                gpi.verificaIdoneita();
+            }
+        }
+    }
+
+    public ArrayList<PreferenzaInvitato> getPref() {
+        return pref;
+    }
+
+}
